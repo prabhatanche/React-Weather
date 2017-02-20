417a47dc98a5d03fb89915de76d0eb051b0a3540
prabhat anche <prabhat anche>
2017-02-20 05:36:42 -0500

    more styles

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Contact = (props) => {
     return (
         <div>
-            <h1 className="text-center">Contact @</h1>
+            <h1 className="text-center" >Contact @</h1>
             <p>Call ME@:- (810)-777-3345</p>
             <p>E-mail ME@:- prabhatanche@gmail.com</p>
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -11,12 +11,12 @@ var Nav = React.createClass({
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">React Weather App</li>
+            <li  id="text" className="menu-text">React Weather App</li>
             <li>
-              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              <IndexLink to="/"  activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
             </li>
             <li>
-              <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
+              <Link to="/about"  activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
             </li>
             <li>
               <Link to="/contact" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>Contact</Link>
